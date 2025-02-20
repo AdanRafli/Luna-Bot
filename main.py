8d662f7ed4057c0223ac6a08c00dfb0586f83c67
Adan Rafli <44079707+AdanRafli@users.noreply.github.com>
2025-02-20 19:13:43 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import discord
 from discord.ext import commands
 import asyncio
 import os
+import os
+print("DISCORD_TOKEN:", os.getenv("DISCORD_TOKEN"))
 
 # Mengatur intents agar bot bisa membaca pesan, mengetahui server, dan anggota
 intents = discord.Intents.default()
